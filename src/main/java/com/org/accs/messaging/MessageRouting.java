216e07e64526e33b9911002d99190d44f6c406ff
thaimore <volodea.leyciu@gmail.com>
2023-12-07 19:01:05 +0200

    Memory leak fix

--- src/main/java/com/org/accs/messaging/MessageRouting.java
+++ src/main/java/com/org/accs/messaging/MessageRouting.java
@@ -2,20 +2,25 @@ package com.org.accs.messaging;
 
 import com.org.accs.integration.MessageHandler;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static com.org.ma.utils.Constants.PAYMENT_CHANNEL;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MessageRouting extends RouteBuilder {
+
+    @Value(value = "${spring.kafka.consumer.bootstrap-servers}")
+    private String brokerAddress;
 
     private MessageHandler messageHandler;
 
     @Override
     public void configure() {
-        from("kafka:%s?brokers=172.18.0.1:9092".formatted(PAYMENT_CHANNEL))
+        from("kafka:%s?brokers=%s".formatted(PAYMENT_CHANNEL, brokerAddress))
                 .routeId("payment-service")
                 .process(messageHandler);
     }
